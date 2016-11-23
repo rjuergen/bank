979a0bf3f890c52955e39e234cf50b25b4749c52
Juergen Reichl <reichljuergen@googlemail.com>
2016-11-23 18:40:12 +0100

    TransactionService.transfer method implemented

--- src/main/java/de/jreichl/service/TransactionService.java
+++ src/main/java/de/jreichl/service/TransactionService.java
@@ -19,40 +19,60 @@ import javax.transaction.Transactional;
  *
  * @author Jürgen Reichl
  */
-public class TransactionService {
+public class TransactionService {   
     
-
     
     @Inject
     private AccountTransactionRepository accountTransactionRepo;
     
-    @Inject AccountRepository accountRepo;
+    @Inject 
+    private AccountRepository accountRepo;
     
+    /**
+     * method to transfer amount in cent from one account(fromIBAN) to another(toIBAN)
+     * @param amountInCent amount in cent
+     * @param fromIBAN the account to debit(-)
+     * @param toIBAN the account to credit(+)
+     * @return true if account transactions were successfully
+     * @throws TransactionFailedException 
+     */
     @Transactional
-    public AccountTransaction createTransaction(long amountInCent, TransactionType type, String IBAN) throws TransactionFailedException {        
+    public boolean transfer(long amountInCent, String fromIBAN, String toIBAN) throws TransactionFailedException {        
          // get current/transaction date
         Date currentDate = new Date();
         
-        try {     
-            Account account = null;
+        try {
+            Account fromAccount = null;
+            Account toAccount = null;
             try{
                 // get account with given IBAN
-                account = accountRepo.findByIBAN(IBAN);     
+                fromAccount = accountRepo.findByIBAN(fromIBAN);     
             } catch (NoResultException nrex) {
-                throw new TransactionFailedException(nrex, String.format("Transaction failed! %s is not a valid IBAN", IBAN), IBAN, currentDate, type, amountInCent);
+                throw new TransactionFailedException(nrex, String.format("Transaction failed! %s is not a valid IBAN", fromIBAN), fromIBAN, toIBAN, currentDate, amountInCent);
+            }
+            
+            try{
+                // get account with given IBAN
+                toAccount = accountRepo.findByIBAN(toIBAN);     
+            } catch (NoResultException nrex) {
+                throw new TransactionFailedException(nrex, String.format("Transaction failed! %s is not a valid IBAN", toIBAN), fromIBAN, toIBAN, currentDate, amountInCent);
             }
 
             // create Transaction
-            AccountTransaction t = new AccountTransaction(account, type, amountInCent, new java.sql.Date(currentDate.getTime()));        
+            AccountTransaction t1 = new AccountTransaction(fromAccount, TransactionType.DEBIT, amountInCent, new java.sql.Date(currentDate.getTime()));        
 
-            // persist
-            accountTransactionRepo.persist(t);
+            AccountTransaction t2 = new AccountTransaction(toAccount, TransactionType.CREDIT, amountInCent, new java.sql.Date(currentDate.getTime()));
             
-            return t;
+            // persist
+            accountTransactionRepo.persist(t1);
+            accountTransactionRepo.persist(t2);
+            
+            return true;
         } catch(TransactionFailedException ex) {
             throw ex;
         } catch(Exception ex) {
-            throw new TransactionFailedException(ex, "Transaction failed!", IBAN, currentDate, type, amountInCent);
+            throw new TransactionFailedException(ex, "Transaction failed!", fromIBAN, toIBAN, currentDate, amountInCent);
         }
     }
+        
 }
--- src/main/java/de/jreichl/service/exceptions/TransactionFailedException.java
+++ src/main/java/de/jreichl/service/exceptions/TransactionFailedException.java
@@ -4,7 +4,6 @@
  */
 package de.jreichl.service.exceptions;
 
-import de.jreichl.jpa.entity.type.TransactionType;
 import java.util.Date;
 
 
@@ -13,33 +12,35 @@ import java.util.Date;
  * @author Jürgen Reichl
  */
 public class TransactionFailedException extends Exception {
-    private final String IBAN;
+    private final String fromIBAN;
+    private final String toIBAN;
     private final Date transactionDate;
-    private final TransactionType type;
     private final long amountInCent;
 
-    public TransactionFailedException(Exception ex, String message, String IBAN, Date transactionDate, TransactionType type, long amountInCent) {
+    public TransactionFailedException(Exception ex, String message, String fromIBAN, String toIBAN, Date transactionDate, long amountInCent) {
         super(message, ex);
-        this.IBAN = IBAN;
-        this.transactionDate = transactionDate;
-        this.type = type;
+        this.fromIBAN = fromIBAN;
+        this.toIBAN = toIBAN;
+        this.transactionDate = transactionDate;        
         this.amountInCent = amountInCent;
     }
 
-    public String getIBAN() {
-        return IBAN;
+    public String getFromIBAN() {
+        return fromIBAN;
+    }
+
+    public String getToIBAN() {
+        return toIBAN;
     }
 
     public Date getTransactionDate() {
         return transactionDate;
     }
 
-    public TransactionType getType() {
-        return type;
-    }
-
     public long getAmountInCent() {
         return amountInCent;
     }
+
+
 
 }
